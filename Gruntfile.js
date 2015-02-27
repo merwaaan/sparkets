7525421f39e7a55b294949d05b879551671fba75
Merwan Achibet <achibetmerwan@gmail.com>
2015-02-27 18:05:43 +0100

    - replace make with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-vows');
+
+  var serverScripts = ['src/server/*.coffee', 'src/*.coffee'];
+  var clientScripts = ['src/client/*.coffee', 'src/*.coffee'];
+
+  grunt.initConfig({
+    
+    
+
+    coffee: {
+      options: {
+        join: true
+      },
+      server: {
+        files: {'build/server.js': serverScripts}
+      },
+      client: {
+        files: {'build/client.js': clientScripts}
+      }
+    },
+
+    vows: {
+      all: {
+        options: {
+          verbose: true
+        },
+        src: ['test/*.coffee']
+      }
+    },
+
+    clean: {
+      all: ['build']
+    },
+
+    watch: {
+      server: {
+        files: serverScripts,
+        tasks: ['coffee:server']
+      },
+      client: {
+        files: clientScripts,
+        tasks: ['coffee:client']
+      }
+    }
+
+  });
+
+  grunt.registerTask('default', ['clean', 'both', 'watch']);
+
+  grunt.registerTask('server', ['coffee:server']);
+  grunt.registerTask('client', ['coffee:client']);
+  grunt.registerTask('both', ['server', 'client']);
+
+  grunt.registerTask('test', ['vows:all']);
+
+};
